7b8fb75a04985993e037ed21aac1537754150f46
anitjee <anitsamraju@gmail.com>
2017-06-11 20:03:23 +0000

    Added listing and layout links

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,15 @@
 class AssignmentsController < ApplicationController
   
+  def index
+   @assignments = Assignment.all 
+  end
+  
   def new
     @assignment = Assignment.new
+  end
+  
+   def edit
+    @assignment = Assignment.find(params[:id]) 
   end
   
   def create
@@ -9,24 +17,30 @@ class AssignmentsController < ApplicationController
   # render plain: params[:assignments].inspect
   @assignment = Assignment.new(assignment_params)
   
-  if @assignment.save
-  flash[:notice] = "Article was successfully created"
-  redirect_to assignment_path(@assignment) #redirect to show action
+    if @assignment.save
+       flash[:notice] = "Article was successfully created"
+       redirect_to assignment_path(@assignment) #redirect to show action
   
-  else
-    render 'new'
-    
+     else
+       render 'new'
+     end
     end
-  end
-  
-  private
-  def assignment_params
- params.require(:assignment).permit(:title, :description) 
-  end
-  def show
-    @assignment = Assignment.find(params[:id])
-  
-  end
     
-  
+       def update
+       @assignment = Assignment.find(params[:id])
+          if @assignment.update(assignment_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to assignment_path(@assignment)
+          else
+            render 'edit'
+          end
+    end
+        private
+         def assignment_params
+         params.require(:assignment).permit(:title, :description) 
+        end
+
+     def show
+    @assignment = Assignment.find(params[:id])
+  end
 end
